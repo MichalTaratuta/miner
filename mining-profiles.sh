35059d2438dd48da35cf027c12e92760e07a8e90
MichalTaratuta <m.taratuta@hotmail.com>
2021-02-20 08:26:34 +0000

    lower down OC on memeory due to crashes add stderr reporting to logs

--- mining-profiles.sh
+++ mining-profiles.sh
@@ -15,7 +15,7 @@ fanAuto() {
     fanControlState=$(nvidia-settings -q [gpu:$gpuId]/GPUFanControlState | grep '^  Attribute' | grep "gpu:$gpuId" | perl -pe 's/^.*?(\d)\.*$/\1/;')
     if [ $fanControlState -ne 0 ]; then
             eval "nvidia-settings -a [gpu:$gpuId]/GPUFanControlState=0" > /dev/null
-            echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Current Fan Speed: ${GREEN}$fanSpeed %${NC}, Setting Fan Speed to ${BYELLOW}Auto${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+            echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Current Fan Speed: ${GREEN}$fanSpeed %${NC}, Setting Fan Speed to ${BYELLOW}Auto${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     fi
 }
 
@@ -25,11 +25,11 @@ powerCap() {
     powerLimit=$(nvidia-smi --query-gpu=power.limit --format=csv,noheader,nounits --id=$gpuId | cut -d'.' -f1)
 
     if [ $powerLimit != $1 ]; then
-        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Setting power cap to ${BYELLOW}$1W${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Setting power cap to ${BYELLOW}$1W${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
         eval "sudo nvidia-smi --id=$gpuId --power-limit=$1" > /dev/null
 
     else
-        echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Power cap already at ${GREEN}$1 W${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+        echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Power cap already at ${GREEN}$1 W${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     fi
 }
 
@@ -38,7 +38,7 @@ cleanup() {
         # Get number of GPUs
         numGPUs=$(nvidia-smi --query-gpu=count --format=csv,noheader -id=0)
 
-        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}Executing Trap${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}Executing Trap${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
 
         # Loop through each GPU
         for gpuId in $(seq 0 $((numGPUs-1))); do
@@ -47,16 +47,16 @@ cleanup() {
             echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting Fan Speed to Auto${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
 
             # GPUPowerMizerMode=0 is Adaptive Mode
-            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting automatic P-State control${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting automatic P-State control${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
             eval "nvidia-settings --assign [gpu:$gpuId]/GPUPowerMizerMode=0" > /dev/null
 
-            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting GPU clock offset to 0MHz${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting GPU clock offset to 0MHz${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
             eval "nvidia-settings --assign [gpu:$gpuId]/GPUMemoryTransferRateOffset[4]=0" > /dev/null
 
-            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting GPU memory offset to 0MHz${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$gpuId, Setting GPU memory offset to 0MHz${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
             eval "nvidia-settings --assign [gpu:$gpuId]/GPUMemoryTransferRateOffset[4]=0" > /dev/null
         done
-        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}Exit${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}Exit${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
         exit
 }
 
@@ -78,21 +78,21 @@ setFanSpeed() {
 
     if [ $fanSpeed -ne $1 ]; then
         eval "nvidia-settings --assign [gpu:$gpuId]/GPUFanControlState=1 --assign [fan:0]/GPUTargetFanSpeed=$1 --assign [fan:1]/GPUTargetFanSpeed=$1" > /dev/null
-        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Current Fan Speed: ${GREEN}$fanSpeed%${NC}, Setting Fan Speed to ${BYELLOW}$1%${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+        echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Current Fan Speed: ${GREEN}$fanSpeed%${NC}, Setting Fan Speed to ${BYELLOW}$1%${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     else
-        echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Fan Speed already at: ${GREEN}$fanSpeed%${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+        echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Fan Speed already at: ${GREEN}$fanSpeed%${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     fi
 }
 
 gpuClockOffset() {
     gpuId=$2
-    echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Setting GPU clock offset to ${BYELLOW}$1MHz${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+    echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Setting GPU clock offset to ${BYELLOW}$1MHz${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     eval "nvidia-settings --assign [gpu:$gpuId]/GPUGraphicsClockOffset[4]=$1" > /dev/null
 }
 
 memRateOffset() {
     gpuId=$2
-    echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Setting GPU memory offset to ${BYELLOW}$1MHz${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+    echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${BYELLOW}GPU:$gpuId${NC}, Setting GPU memory offset to ${BYELLOW}$1MHz${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     eval "nvidia-settings --assign [gpu:$gpuId]/GPUMemoryTransferRateOffset[4]=$1" > /dev/null
 }
 
@@ -126,7 +126,7 @@ tempControl() {
     #Executing thermal control
     if [ ! -z ${thermalThrottle[@]} ]; then
         for id in ${thermalThrottle[@]}; do
-            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$id, Thermal Throttle Active, Executing Auto Fans${NC}" | tee -a "/home/omen/mining-profiles-$(date +%Y%m%d).log"
+            echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${RED}GPU:$id, Thermal Throttle Active, Executing Auto Fans${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%Y%m%d).log"
             fanAuto $id
         done
         sleep 90
@@ -161,12 +161,12 @@ declare -A defaultFanSpeed
 profile() {
     gpuId=$2
     defaultFanSpeed[$gpuId]=$3
-    echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Enabling mode: ${GREEN}$1${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+    echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, Enabling mode: ${GREEN}$1${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
     setFanSpeed ${defaultFanSpeed[$gpuId]} $gpuId
     powerCap $4 $gpuId
     gpuClockOffset $5 $gpuId
     memRateOffset $6 $gpuId
-    echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, ${BYELLOW}$1 mode enabled${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+    echo -e "$(date +'%d-%m-%Y %H:%M:%S') GPU:$gpuId, ${BYELLOW}$1 mode enabled${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
 }
 
 #Menu
@@ -185,8 +185,8 @@ mainMenu() {
                     profile "Extension Cold" "0" "75" "125" "-300" "2500"
                     profile "Extension Cold" "1" "75" "125" "-300" "2500"
                 else
-                    # ~98 Mh
-                    profile "Extension Cold" "0" "75" "230" "-400" "2300"
+                    # ~96 Mh
+                    profile "Extension Cold" "0" "75" "230" "-400" "2000"
                 fi
                 break
             ;;
@@ -197,7 +197,7 @@ mainMenu() {
                     profile "Extension Warm" "1" "85" "125" "-400" "2500"
                 else
                     # ~96 Mh
-                    profile "Extension Warm" "0" "85" "230" "-400" "2300"
+                    profile "Extension Warm" "0" "85" "230" "-400" "2000"
                 fi
                 break
                 ;;
@@ -207,7 +207,7 @@ mainMenu() {
                     profile "Extension Warm" "0" "90" "125" "-400" "2500"
                     profile "Extension Warm" "1" "90" "125" "-400" "2500"
                 else
-                    profile "Extension Hot" "0" "90" "230" "-400" "2300"
+                    profile "Extension Hot" "0" "90" "230" "-400" "2000"
                 fi
                 break
             ;;
@@ -215,7 +215,7 @@ mainMenu() {
                 exit
             ;;
             *)
-                echo -e "${RED}Invalid Option${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+                echo -e "${RED}Invalid Option${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
                 exit
             ;;
         esac
@@ -223,7 +223,7 @@ mainMenu() {
 }
 mainMenu
 
-echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${GREEN}Running Temp Control${NC}" | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
+echo -e "$(date +'%d-%m-%Y %H:%M:%S') ${GREEN}Running Temp Control${NC}" 2>&1 | tee -a "/home/omen/mining-profiles-$(date +%d%m%Y).log"
 while :
 do
     tempControl
